b1d3dcdeb4b8e236d5c52f84475e1629ffed9caf
Robert Ellul <rob.ellul@gmail.com>
2023-08-24 20:16:32 +0200

    Added a SearchByCategory class to be used as a filter in the DisplayUsers view

---
+++ AccessControlApplication/Models/SearchByCategory.cs
@@ -0,0 +1,22 @@
+﻿namespace AccessControlApplication.Models
+{
+    public class SearchByCategory
+    {
+        private static string? search = "";
+        
+        private static bool? getAllData = true;
+
+        public string? Search
+        {
+            set { search = value; }
+            get { return search; }
+        }
+        
+        public bool? GetAllData
+        {
+            set {  getAllData = value; } 
+            get { return getAllData; } 
+        }
+
+    }
+}
