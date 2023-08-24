d8523d8ed3a29a6af26b2bfe525ad148385f1be0
Robert Ellul <rob.ellul@gmail.com>
2023-08-24 20:26:13 +0200

    Added the functionality for the DisplayUsers View

--- AccessControlApplication/Models/CombinedClasses.cs
+++ AccessControlApplication/Models/CombinedClasses.cs
@@ -2,8 +2,12 @@
 {
     public class CombinedClasses
     {
+        private List<Register>? registeredUsers;
         public Register? RegisterUser { get; set; }
         public ButtonControls? ButtonSync { get; set; }
         public LoggedUser ? User { get; set; }
+        public SearchByCategory? Category { get; set; }
+
+        public List<Register>? RegisteredUsers { get; set; }
     }
 }
