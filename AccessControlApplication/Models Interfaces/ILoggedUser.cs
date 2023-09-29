fca782d766338269e8d53c8e5fcd8b69542e17a0
Robert Ellul <rob.ellul@gmail.com>
2023-09-29 20:37:46 +0200

    Added UserName Property

--- AccessControlApplication/Models Interfaces/ILoggedUser.cs
+++ AccessControlApplication/Models Interfaces/ILoggedUser.cs
@@ -3,6 +3,7 @@
     public interface ILoggedUser
     {
         public bool UserCheck {  get; set; }
+        public string UserName { get; set; }
         int CurrentUser { get; set; }
 
         bool AdminRights { get; set; }
